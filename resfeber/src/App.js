dd5f53534c6ead383d0bde67ff18753b0544853a
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-10-03 21:52:39 -0400

    deploy to heroku

--- resfeber/src/App.js
+++ resfeber/src/App.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import About from "./Pages/About";
-import Dashboard from "./Pages/Dashboard";
-import Landing from "./Pages/Landing";
-import LogIn from "./Pages/LogIn";
-import SignUp from "./Pages/SignUp";
+// import Dashboard from "./Pages/Dashboard";
+// import Landing from "./Pages/Landing";
+// import LogIn from "./Pages/LogIn";
+// import SignUp from "./Pages/SignUp";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Landing}/>
+          {/* <Route exact path="/" component={Landing}/> */}
           <Route exact path="/about" component={About}/>
-          <Route exact path="/dashboard" component={Dashboard}/>
+          {/* <Route exact path="/dashboard" component={Dashboard}/>
           <Route exact path="/login" component={LogIn}/>
           <Route exact path="/signup" component={SignUp}/>
-          <Route component={Dashboard}/>
+          <Route component={Dashboard}/> */}
           
         </div>
       </Router>
---
+++ resfeber/src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Navbar from "../../components/Navbar";
+
+class About extends Component {
+    render() {
+        return (
+            <Navbar/>
+     )}
+}
+
+export default About;
