4ae8787f6762dda8d3a2b59f6e009f111be5a462
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-10-11 16:12:32 -0400

    dashboard/card css

--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import {Card} from 'react-materialize'
 import {CardTitle} from 'react-materialize'
-import {Col} from 'react-materialize'
 import Navbar from "../../components/Navbar";
 
 
@@ -11,21 +10,21 @@ class Dashboard extends Component {
         return (
             <div>
                 <Navbar />
-                <div>
-                    <Col m={6} >
+                <div className="row">
+                    <div className="col s6" >
                         <Card className='small'
                             header={<CardTitle image='./images/worldMap.png'>Card Title</CardTitle>}
                             actions={[<a href='/dashboard'>This is a Link</a>]}>
                             I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
                         </Card>
-                    </Col>
-                    <Col m={6} >
+                    </div>
+                    <div className="col s6" >
                         <Card className='small'
                             header={<CardTitle image='./images/worldMap.png'>Card Title</CardTitle>}
                             actions={[<a href='/dashboard'>This is a Link</a>]}>
                             I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
                         </Card>
-                    </Col>
+                    </div>
                 </div>
             </div>
         )
